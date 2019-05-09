68422a6fc85bf4ccaea7d8ae1e94147dc3a9773b
Lara Eller <lareh_eller@yahoo.com>
2019-05-09 18:02:42 -0400

    set up and export the orm

---
+++ config/orm.js
@@ -0,0 +1,27 @@
+var connection = require("./connection.js");
+
+var orm = {
+    selectAll: function(cb) {
+        connection.query("SELECT * FROM burgers", function(err, res) {
+            if (err) throw err;
+            cb(res);
+        });
+    },
+    insertOne: function(burgerName, cb) {
+        var queryString = "INSERT INTO burgers (burger_name) VALUES (?)";
+        connection.query(queryString, burgerName, function(err, res) {
+            if (err) throw err;
+            cb(res)
+        });
+    },
+    updateOne: function(id, cb) {
+        var queryString = "UPDATE burgers SET devoured = true WHERE id = ?";
+        connection.query(queryString, id, function(err,res) {
+            if (err) throw err;
+            cb(res)
+        });
+    }
+};
+
+module.exports = orm;
+
