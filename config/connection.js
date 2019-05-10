f62e7dbee604d786a68e063ff33b693e2b0acc9c
Lara Eller <lareh_eller@yahoo.com>
2019-05-10 12:16:54 -0400

    change db

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,7 @@
 require('dotenv').config();
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_DATABASE,
-});
+var connection = mysql.createConnection({JAWSDB_URL});
 
 connection.connect(function(err) {
     if (err) {
