d358aa3d8869b53703e1fb9fd5f2a0a8b40be0fa
Lara Eller <lareh_eller@yahoo.com>
2019-05-10 00:07:28 -0400

    Update database for Heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,26 @@
 // require('dotenv').config();
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-});
+if (process.env.CLEARDB_DATABASE_URL) {
+    connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "",
+        password: "root",
+        database: "burgers_db"
+    });
+}
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) {
-      console.error(err);
-      return;
+        console.error(err);
+        return;
     }
     console.log("connected as id " + connection.threadId);
-  });
-  
-  // Export connection for our ORM to use.
-  module.exports = connection;
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
