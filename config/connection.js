22a3669014e8a5a44942d7c63c262b188333b343
Lara Eller <lareh_eller@yahoo.com>
2019-05-10 00:39:48 -0400

    config database for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,26 +1,20 @@
-// require('dotenv').config();
+require('dotenv').config();
 var mysql = require("mysql");
-var connection;
 
-if (process.env.CLEARDB_DATABASE_URL) {
-    connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
-} else {
-    connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: "",
-        password: "root",
-        database: "burgers_db"
-    });
-}
-
-connection.connect(function (err) {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
+var connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE
 });
 
-// Export connection for our ORM to use.
-module.exports = connection;
+connection.connect(function(err) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+  
+  // Export connection for our ORM to use.
+  module.exports = connection;
