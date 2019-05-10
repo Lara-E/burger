64da189f17c96dee8d087af18961d3ec964278fe
Lara Eller <lareh_eller@yahoo.com>
2019-05-10 01:28:41 -0400

    db config

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,8 @@ var connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_DATABASE
+    database: process.env.DB_DATABASE,
+    connectionLimit: 10
 });
 
 connection.connect(function(err) {
